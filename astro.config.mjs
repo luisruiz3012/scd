a1096b321ffad75d5359edb25e76758abc0489a8
luisruiz3012 <luisdiegoaguilar@outlook.com>
2026-05-25 16:50:20 -0600

    Fix: agregar .gitignore, instalar @astrojs/vercel, limpiar node_modules del repo, mover TECNOLOGIAS DE ESCANEO a inicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://scd.com.gt',
+  adapter: vercel(),
   output: 'static',
   build: {
     format: 'file'
